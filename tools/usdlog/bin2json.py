feb402678312ed7b8237541ef29678304fb62ab6
tungnx94 <xtungnguyen94@gmail.com>
2018-08-20 21:53:49 +0200

    add function to save log to csv

--- tools/usdlog/bin2json.py
+++ tools/usdlog/bin2json.py
@@ -2,31 +2,44 @@ import CF_functions as cff
 import json
 import argparse
 
+import pandas as pd
+
 logfile = "testLog"
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Convert a binary logfile to json.')
-    parser.add_argument('in_path', metavar='infile',
+    parser = argparse.ArgumentParser(
+        description='Convert a binary logfile to json.')
+    parser.add_argument('-i', '--input', metavar='infile',
                         help='The binary file that is converted to json.')
 
-    parser.add_argument('out_path', metavar='outfile', nargs='?',
+    parser.add_argument('-o', '--output', metavar='outfile', nargs='?',
                         help='The output json file.')
 
     args = parser.parse_args()
 
-    in_path = args.in_path
-    out_path = args.out_path if args.out_path is not None else args.in_path + ".json"
+    in_path = args.input
+    output = args.output if args.output is not None else args.input + ".json"
 
     logData = cff.decode(in_path)
 
-    for key in logData.keys():
-        logData[key] = logData[key].tolist()
+    logData_list = {}
 
-    f = open(out_path, "w")
-    print("Number of lines written to {}: {}".format(out_path, str(f.write(json.dumps(logData)))))
+    for key in logData.keys():
+        logData_list[key] = logData[key].tolist()
+
+    f = open(output, "w")
+    print("Number of lines written to {}: {}".format(
+        output, str(f.write(json.dumps(logData_list)))))
     f.close()
 
+    # TODO: write to csv
+    out_csv = args.input + ".csv"
+
+    data_frame = pd.DataFrame.from_dict(logData)
+    data_frame.to_csv(out_csv)
+
+    print("Saved data to {}".format(out_csv))
 
 if __name__ == "__main__":
     main()
